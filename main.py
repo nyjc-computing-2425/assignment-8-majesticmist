506077ce5771174e72d63eae9d0c79e75780a3cd
Steve Nguyen <154491875+majesticmist@users.noreply.github.com>
2024-04-08 20:31:34 +0000

    Final Submission?

--- main.py
+++ main.py
@@ -1,2 +1,34 @@
 # Built-in imports
+def reverse(text):
+  """
+  text: string
+  return: boolean
 
+  function reverse string
+  
+  
+  """
+  
+  if len(text)>0:
+    return reverse(text[1:]) + text[0]
+  else: 
+    return ''
+
+
+def is_palindrome(whatever):
+  """
+  
+  whatever: string
+  return: boolean
+
+  True if palindrome
+  """
+  if len(whatever) <= 1:
+    return True
+    
+  if len(whatever) > 0:
+    if whatever[0].lower == whatever[-1].lower:
+      return is_palindrome(whatever[1:-1]) 
+    else:
+      return False
+  
